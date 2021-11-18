a27e355ea8df21eae97a0a0d8da155047a7dc5ea
Alexandersaur <jeremy.a.steward@gmail.com>
2021-11-18 13:05:45 -0500

    code Challenge

--- js/site.js
+++ js/site.js
@@ -3,12 +3,14 @@
     //--Get values from the page--//
     let fizzValue = document.getElementById("fizzValue").value;
     let buzzValue = document.getElementById("buzzValue").value;
+    let arraySize = document.getElementById("arraySize").value;
     //--Ensure values are integers (input validation)--//
     fizzValue = parseInt(fizzValue);
     buzzValue = parseInt(buzzValue);
-    if(Number.isInteger(fizzValue) && Number.isInteger(buzzValue)){
+    arraySize = parseInt(arraySize);
+    if(Number.isInteger(fizzValue) && Number.isInteger(buzzValue) && Number.isInteger(arraySize)){
         //--Call fizzBuzz function--//
-        let fbArray = fizzBuzz(fizzValue, buzzValue);
+        let fbArray = fizzBuzz(fizzValue, buzzValue, arraySize);
         //--Call displayNumbers function--//
         displayNumbers(fbArray);
     }else{
@@ -16,11 +18,52 @@
     }
 }
 
-//--Evaluate function using input values (logic function)--//
-function fizzBuzz(fizzValue, buzzValue){
+  //--Get the values from the user (controller function)--//
+  function getValuesB(){
+    //--Get values from the page--//
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
+    let arraySize = document.getElementById("arraySize").value;
+    //--Ensure values are integers (input validation)--//
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
+    arraySize = parseInt(arraySize);
+    if(Number.isInteger(fizzValue) && Number.isInteger(buzzValue) && Number.isInteger(arraySize)){
+        //--Call fizzBuzz function--//
+        let fbArray = fizzBuzzB(fizzValue, buzzValue, arraySize);
+        //--Call displayNumbers function--//
+        displayNumbers(fbArray);
+    }else{
+        alert("You must enter integers");
+    }
+}
+
+  //--Get the values from the user (controller function)--//
+  function getValuesC(){
+    //--Get values from the page--//
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
+    let arraySize = document.getElementById("arraySize").value;
+    //--Ensure values are integers (input validation)--//
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
+    arraySize = parseInt(arraySize);
+    if(Number.isInteger(fizzValue) && Number.isInteger(buzzValue) && Number.isInteger(arraySize)){
+        //--Call fizzBuzz function--//
+        let fbArray = fizzBuzzC(fizzValue, buzzValue, arraySize);
+        //--Call displayNumbers function--//
+        displayNumbers(fbArray);
+    }else{
+        alert("You must enter integers");
+    }
+}
+
+//--Option A: Evaluate function using input values (logic function)--//
+function fizzBuzz(fizzValue, buzzValue, arraySize){
+    var startTime = performance.now();
     let returnArray = [];
-    //--Get all numbers from 0 to 100--//
-    for(let i = 1; i <= 100; i++){
+    //--Get all numbers from 0 to arraySize--//
+    for(let i = 1; i <= arraySize; i++){
          //--Check if number is Fizz/Buzz/FizzBuzz--//
         if((i % fizzValue == 0) && (i % buzzValue == 0)){
             returnArray.push('FizzBuzz');
@@ -32,17 +75,23 @@ function fizzBuzz(fizzValue, buzzValue){
             returnArray.push(i);
         }
     }
+    var endTime = performance.now();
+    var totalTime = endTime - startTime;
+    //--Select and populate the timing section from the page--//
+    let functionTime = document.getElementById("timeHeader");
+    functionTime.innerHTML = "Results: Option A took " + `${totalTime}` + " milliseconds.";
     return returnArray;
 }
 
-//--Alternate solution without if/else statements (logic function)--//
-function fizzBuzzB(fizzValue, buzzValue){
+//--Option B: Alternate solution without if/else statements (logic function)--//
+function fizzBuzzB(fizzValue, buzzValue, arraySize){
+    var startTime = performance.now();
     let returnArray = [];
     //--Set default boolean logic for Fizz and Buzz--//
     let Fizz = false;
     let Buzz = false;
     //--Loop through values, comparing to inputs--//
-    for(let i = 1; i <= 100; i++){
+    for(let i = 1; i <= arraySize; i++){
         Fizz = i % fizzValue == 0;
         Buzz = i % buzzValue == 0;
         //--Switch false to true if requirements are met--//
@@ -62,6 +111,43 @@ function fizzBuzzB(fizzValue, buzzValue){
             }
         }
     }
+    var endTime = performance.now();
+    var totalTime = endTime - startTime;
+    //--Select and populate the timing section from the page--//
+    let functionTime = document.getElementById("timeHeader");
+    functionTime.innerHTML = "Results: Option B took " + `${totalTime}` + " milliseconds.";
+    return returnArray;
+}
+
+//--Option C: Alternate solution without using '%' modulus operator (logic function)--//
+function fizzBuzzC(fizzValue, buzzValue, arraySize){
+    var startTime = +new Date();
+    let returnArray = [];
+    //--Set initial Fizz and Buzz values--//
+    let Fizz = 1;
+    let Buzz = 1;
+    //--Get all numbers from 0 to arraySize while incrementing Fizz and Buzz--//
+    for(let i = 1; i <= arraySize; i++, Fizz++, Buzz++){
+        //--Check if number is Fizz/Buzz/FizzBuzz, then reset incrementation--//
+       if((Fizz == fizzValue) && (Buzz == buzzValue)){
+           returnArray.push('FizzBuzz');
+           Fizz = 0;
+           Buzz = 0;
+       }else if(Fizz == fizzValue){
+           returnArray.push('Fizz');
+           Fizz = 0;
+       }else if(Buzz == buzzValue){
+           returnArray.push('Buzz');
+           Buzz = 0;
+       }else{
+           returnArray.push(i);
+       }
+    }
+    var endTime = +new Date();
+    var totalTime = endTime - startTime;
+    //--Select and populate the timing section from the page--//
+    let functionTime = document.getElementById("timeHeader");
+    functionTime.innerHTML = "Results: Option C took " + `${totalTime}` + " milliseconds.";
     return returnArray;
 }
 
